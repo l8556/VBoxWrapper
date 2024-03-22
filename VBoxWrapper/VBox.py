5f0e314c2553507edc1f71b13ac2ca037f7d829a
Nikolai Lambrov <52069739+l8556@users.noreply.github.com>
2024-03-22 18:23:29 +0300

    add description (#2)
    * add description
    
    * add description for network_adapter methods
    
    * add description for FileUtils methods
    
    * add description for snapshot methods
    
    * add description for virtualmachine methods
    
    * add description for VBox methods

--- VBoxWrapper/VBox.py
+++ VBoxWrapper/VBox.py
@@ -8,8 +8,16 @@ from subprocess import getoutput
 
 
 class Vbox:
+    """
+    Class for interacting with VirtualBox and managing virtual machines.
+    """
 
     def vm_list(self, group_name: str = None) -> list[list[str]]:
+        """
+        Get a list of virtual machines along with their UUIDs.
+        :param group_name: Filter virtual machines by group name.
+        :return: List of virtual machine names and UUIDs. [[name, uuid]]
+        """
         vm_list = [
             [vm[0].replace('"', ''), vm[1].translate(str.maketrans('', '', '{}'))]
             for vm in [vm.split() for vm in getoutput(cmd.list).split('\n')]
@@ -19,6 +27,11 @@ class Vbox:
         return vm_list
 
     def check_vm_names(self, vm_names: list | str) -> list | str:
+        """
+        Check if the provided virtual machine names exist.
+        :param vm_names: List or string of virtual machine names.
+        :return: List or string of virtual machine names.
+        """
         existing_names = self.get_vm_names()
         for name in [vm_names] if isinstance(vm_names, str) else vm_names:
             if name not in existing_names:
@@ -26,16 +39,35 @@ class Vbox:
         return vm_names
 
     def get_vm_names(self, group_name: str = None) -> list:
+        """
+        Get a list of virtual machine names.
+        :param group_name: Filter virtual machines by group name.
+        :return: List of virtual machine names.
+        """
         return [vm[0] for vm in self.vm_list(group_name)]
 
     def get_vm_uuids(self, group_name: str = None) -> list:
+        """
+        Get a list of virtual machine UUIDs.
+        :param group_name: Filter virtual machines by group name.
+        :return: List of virtual machine UUIDs.
+        """
         return [vm[1] for vm in self.vm_list(group_name)]
 
     @staticmethod
     def get_group_list() -> list:
+        """
+        Get a list of available groups.
+        :return: List of group names.
+        """
         return [basename(group) for group in getoutput(cmd.group_list).replace('"', '').split('\n')]
 
     def check_group_name(self, group_name: str) -> str:
+        """
+        Checks if the group exists in the Vbox
+        :param group_name: Group name to check.
+        :return: Group name if exists.
+        """
         existing_names = self.get_group_list()
         if group_name in existing_names:
             return group_name
--- VBoxWrapper/VirtualMachine/snapshot.py
+++ VBoxWrapper/VirtualMachine/snapshot.py
@@ -8,26 +8,51 @@ console = Console()
 print = console.print
 
 class Snapshot:
+    """
+    Class to manage snapshots of a virtual machine.
+    """
+
     _cmd = Commands()
 
     def __init__(self, vm_id: str):
         self.name = vm_id
 
     def list(self) -> list:
+        """
+        Get a list of snapshots for the virtual machine.
+        :return: List of snapshots.
+        """
         return self._cmd.get_output(f"{self._cmd.snapshot} {self.name} list").split('\n')
 
     def delete(self, name: str) -> None:
+        """
+        Delete a snapshot.
+        :param name: Name of the snapshot to delete.
+        """
         self._cmd.run(f"{self._cmd.snapshot} {self.name} delete {name}")
         print(f"[green]|INFO| Snapshot [cyan]{name}[/] deleted.")
 
     def restore(self, name: str = None) -> None:
+        """
+        Restore a snapshot.
+        :param name: Name of the snapshot to restore. If None, restore the most recent snapshot.
+        """
         print(f"[green]|INFO|{self.name}| Restoring snapshot: {name if name else self.list()[-1].strip()}")
         self._cmd.run(f"{self._cmd.snapshot} {self.name} {f'restore {name}' if name else 'restorecurrent'}")
         time.sleep(1)  # todo
 
     def rename(self, old_name: str, new_name: str) -> None:
+        """
+        Rename a snapshot.
+        :param old_name: Current name of the snapshot.
+        :param new_name: New name for the snapshot.
+        """
         self._cmd.run(f"{self._cmd.snapshot} {self.name} edit {old_name} --name {new_name}")
         print(f"[green]|INFO| Snapshot [cyan]{old_name}[/] has been renamed to [cyan]{new_name}[/]")
 
     def take(self, name: str) -> None:
+        """
+        Take a snapshot.
+        :param name: Name for the new snapshot.
+        """
         self._cmd.run(f"{self._cmd.snapshot} {self.name} take {name}")

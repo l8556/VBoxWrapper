1c19b823a5924d274413aeed9d13d100c666dd37
l02 <l8556@outlook.com>
2024-03-22 18:22:06 +0300

    add description for VBox methods

--- VBoxWrapper/VBox.py
+++ VBoxWrapper/VBox.py
@@ -8,11 +8,15 @@ from subprocess import getoutput
 
 
 class Vbox:
+    """
+    Class for interacting with VirtualBox and managing virtual machines.
+    """
 
     def vm_list(self, group_name: str = None) -> list[list[str]]:
         """
-        :param group_name:
-        :return: list with list of virtual machine names and uuid: [[name, uuid]]
+        Get a list of virtual machines along with their UUIDs.
+        :param group_name: Filter virtual machines by group name.
+        :return: List of virtual machine names and UUIDs. [[name, uuid]]
         """
         vm_list = [
             [vm[0].replace('"', ''), vm[1].translate(str.maketrans('', '', '{}'))]
@@ -23,6 +27,11 @@ class Vbox:
         return vm_list
 
     def check_vm_names(self, vm_names: list | str) -> list | str:
+        """
+        Check if the provided virtual machine names exist.
+        :param vm_names: List or string of virtual machine names.
+        :return: List or string of virtual machine names.
+        """
         existing_names = self.get_vm_names()
         for name in [vm_names] if isinstance(vm_names, str) else vm_names:
             if name not in existing_names:
@@ -30,20 +39,34 @@ class Vbox:
         return vm_names
 
     def get_vm_names(self, group_name: str = None) -> list:
+        """
+        Get a list of virtual machine names.
+        :param group_name: Filter virtual machines by group name.
+        :return: List of virtual machine names.
+        """
         return [vm[0] for vm in self.vm_list(group_name)]
 
     def get_vm_uuids(self, group_name: str = None) -> list:
+        """
+        Get a list of virtual machine UUIDs.
+        :param group_name: Filter virtual machines by group name.
+        :return: List of virtual machine UUIDs.
+        """
         return [vm[1] for vm in self.vm_list(group_name)]
 
     @staticmethod
     def get_group_list() -> list:
+        """
+        Get a list of available groups.
+        :return: List of group names.
+        """
         return [basename(group) for group in getoutput(cmd.group_list).replace('"', '').split('\n')]
 
     def check_group_name(self, group_name: str) -> str:
         """
         Checks if the group exists in the Vbox
-        :param group_name:
-        :return: group name, if exist
+        :param group_name: Group name to check.
+        :return: Group name if exists.
         """
         existing_names = self.get_group_list()
         if group_name in existing_names:
